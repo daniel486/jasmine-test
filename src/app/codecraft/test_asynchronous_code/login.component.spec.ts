2622b487b9365db793232399271c39a3a5a4e087
Daniel <daniel.felipe.garcia@correounivalle.edu.co>
2018-09-13 13:47:34 -0500

    aditional information for the asynchronous testing

--- src/app/codecraft/test_asynchronous_code/login.component.spec.ts
+++ src/app/codecraft/test_asynchronous_code/login.component.spec.ts
@@ -29,24 +29,34 @@ describe('Component: Login', () => {
         el = fixture.debugElement.query(By.css('a'));
     });
 
+    //Jasmine done function. Para ejecutar la funcion de testeo asincrono de Jasmine se pasa una funcion
+    //como el primer parametro, normalmente llamamos a este parametro done
     it('Text in the button label', (done) => {
         fixture.detectChanges();
         expect(el.nativeElement.textContent.trim()).toBe('Login');
         let spy = spyOn(authService, 'isAuthenticated').and.returnValue(Promise.resolve(true));
         component.ngOnInit();
+        //Añadimos una funcion usando el espia para asegurar que se llame cuando la promesa
+        //retornada desde isAuthenticated este resuelta. En esta funcion sabemos que el componente tiene
+        //el nuevo valor de needsLogin y podemos añadir nuestra espectativa adicional en este punto.
         spy.calls.mostRecent().returnValue.then(() => {
             fixture.detectChanges();
             expect(el.nativeElement.textContent.trim()).toBe('Logout');
+            //Cuando terminen nuestras tareas asincronas le informaremos a Jasmine por la funcion done.
+            //Con esto Jasmine nos dejara crear pruebas asincronas dandonos una funcion done explicita
+            //la que llamaremos cuando la prueba este completa.
             done();
         });
         
     });
 
+    //async and whenStable. Con esto encapsulamos nuestra funcion test spec en otra funcion llamada async.
     it('Text in the button label', async(() => {
         fixture.detectChanges();
         expect(el.nativeElement.textContent.trim()).toBe('Login');
         let spy = spyOn(authService, 'isAuthenticated').and.returnValue(Promise.resolve(true));
         component.ngOnInit();
+        //Ubicamos las pruebas que debamos correr luego de quye la promesa isAuthenticated se resuelva dentro de esta funcion.
         fixture.whenStable().then(() => {
             fixture.detectChanges();
             expect(el.nativeElement.textContent.trim()).toBe('Logout');
@@ -54,12 +64,21 @@ describe('Component: Login', () => {
         
     }));
     
+    //fakeAsync and tick. Encapsulamos la funcion de prueba en una funcion llamada fakeAsync
     it('Text in the button label', fakeAsync(() => {
         fixture.detectChanges();
         expect(el.nativeElement.textContent.trim()).toBe('Login');
         let spy = spyOn(authService, 'isAuthenticated').and.returnValue(Promise.resolve(true));
         component.ngOnInit();
 
+        //Llamamos la funcion tick(), la cual bloquea la ejecucion y simula el paso del tiempo hasta que
+        //todas las actividades asincronas estan completas. donde llamamos tick() todavia hay actividades asincronas
+        //pendientesque queremos completar. Asi como con async, la funcion fakeAsync ejecuta el codigo dentro
+        //de su cuerpo en una zona de testeo especial "fake async". Esto intercepta y mantiene un seguimiento de las promesas
+        //creadas en su cuerpo. De esta manera al llamar a tick() la aplicacion espera por la promesa devuelta desde
+        //isAuthenticated para ser resuelta y entonces permite que la ejecucion pase a la siguiente linea. Luego el codigo
+        //debajo se encuentra de manera lineal, como si estuvieramos ejecutando codigo sincrono, de esta manera no hay
+        //llamadas de vuelta que nos confundan y todo es mas limpio.
         tick();
         
         fixture.detectChanges();
